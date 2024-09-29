3041ba5280c90b9e6ef994407f355fb0dfbcc295
h-sefa <harun.sefa.06@gmail.com>
2024-09-29 16:54:55 +0300

    S14 D2 tamamlandı.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import org.example.model.*;
+import org.example.model.enums.LampType;
+import org.example.model.enums.PaintColor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Bedroom bedroom1 = new Bedroom("Oda1",
+                new Wall("Wall1 oluşturuldu"),
+                new Wall("Wall2 oluşturuldu"),
+                new Wall("Wall3 oluşturuldu"),
+                new Wall("Wall4 oluşturuldu"),
+                new Ceiling(3, PaintColor.WHITE),
+                new Bed("Style 1",4,1,2,1),
+                new Lamp(LampType.NEON,true,5),
+                new Wardrobe(3,2,210.45),
+                new Carpet(1,3, PaintColor.GREEN));
+
+
     }
 }
